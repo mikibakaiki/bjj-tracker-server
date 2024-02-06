0960fcf8e95061c3b5bf47958d17ac66832ab28a
mikibakaiki <joaomiguelcampos6@gmail.com>
2024-02-06 10:39:05 +0000

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); // This enables CORS for all origins
   await app.listen(3000);
 }
 bootstrap();
