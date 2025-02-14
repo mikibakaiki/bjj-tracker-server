bc305ba4ec1ba2fe0039e18b88af561272693a95
mikibakaiki <14791333+mikibakaiki@users.noreply.github.com>
2025-02-14 18:35:32 +0000

    enabling deployment to heroku

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,13 @@ import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Set global prefix for all routes
   app.setGlobalPrefix('api');
+
   app.enableCors(); // This enables CORS for all origins
-  await app.listen(3000);
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
